fcce0e99d31cadd80d9d705efd04b5de33dcc489
madhavavarma.m@gmail.com <madhavavarma.m@gmail.com>
2024-08-22 14:54:58 +0530

    Integration | MUI and MUI Icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
