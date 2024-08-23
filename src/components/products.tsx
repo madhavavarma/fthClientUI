24014ab0a4b3155f9c7f5232fe8af53386ec8e1f
madhavavarma.m@gmail.com <madhavavarma.m@gmail.com>
2024-08-23 17:11:58 +0530

    UI Logic | Products

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -12,34 +12,49 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import InputAdornment from '@mui/material/InputAdornment';
 import { IProduct } from '../models/IProduct';
 import { IOrderDetails } from '../models/IOrderDetails';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
  
 export default function Products() {
 
     const [orderDetails, setOrderDetails] = useState<IOrderDetails[]>([]);
+    const [openDialog, setOpenDialog] = useState(false);
     const [product, setProduct] = useState<IProduct>();
+    const [toremoveProduct, settoRemoveProduct] = useState<IOrderDetails>();
     const [errorMsg, setErrorMsg] = useState("");
     const [quantity, setQuantity] = useState("");
+    const qtyRef = useRef<HTMLInputElement>(null);
 
     const products: IProduct[] = [
         { id: 1, name:'Tomato', measurement: "kg" },
         { id: 2, name:'Kale', measurement: "kg" },
         { id: 3, name:'Spinach', measurement: "kg" },
         { id: 4, name:'Coconuts', measurement: "No" },
-        { id: 5, name:'Potato', measurement: "kg" }       
+        { id: 5, name:'Beetroot', measurement: "kg" },
+        { id: 6, name:'Asparagus', measurement: "kg" },
+        { id: 7, name:'Greens', measurement: "gm" },
+        { id: 8, name:'Okra', measurement: "kg" },
+        { id: 9, name:'Potato', measurement: "kg" }       
       ];
 
     // User selected the product from dropdown to add quantity
     const selectProduct = (event: any, value: any) => {
 
+        event = event;
+
         if(orderDetails.find(orderDetail => orderDetail.productId === value.id)) {
-            setErrorMsg("Product is added to the Order. Edit from the Order List");
+            setErrorMsg("Product already added.");
             return;
         }
 
         setProduct(products.find(product => product.id === value.id));
         setErrorMsg("");
+        
+        if(qtyRef.current) {
+            qtyRef.current.focus()
+        }
     }
 
     // User clicked on Add after selecting product and quantity
@@ -67,56 +82,79 @@ export default function Products() {
         }
     }
 
+    // Remove Confirmation Dialog to Open
+    const openConfirmDialog = (orderDetail: IOrderDetails) => {
+        settoRemoveProduct(orderDetail);
+        setOpenDialog(true);
+    }
+
     // Remove product from User Orders
-    const removeProduct = (id: number) => {
-        var removedOrderDetails = orderDetails.filter(x => x.productId !== id);
+    const removeProduct = () => {
+        var removedOrderDetails = orderDetails.filter(x => toremoveProduct?.productId !== x.productId);
         setOrderDetails(removedOrderDetails);
+        settoRemoveProduct(undefined);
+        setOpenDialog(false);
     }
 
     // Edit product from User Order
-    const editProduct = (orderDetails: IOrderDetails) => {
-        setProduct(products.find(product => product.id === orderDetails.productId));
-        setQuantity(orderDetails.quantity.toString());
-    }
+    // const editProduct = (orderDetails: IOrderDetails) => {
+    //     setProduct(products.find(product => product.id === orderDetails.productId));
+    //     setQuantity(orderDetails.quantity.toString());
+    // }
 
     return (
         <div className="m-4 mt-12 flex flex-col items-center justify-center ">
+
+            {/* Header - Place your order */}
             <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
             <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
             Place your Order
-        </a>
-            <div className="flex flex-row gap-4">
+             </a>
+
+            {/* Order Inputs */}
+             <section className="">
+                <div className="flex flex-row gap-4 mt-8">   
+
                 <Autocomplete
                     disablePortal
                     id="combo-box-demo"
-                    options={products.map(x => ( {id: x.id, label: x.name} ))}
+                    options={products.filter(x => !orderDetails.map(od => od.productId).includes(x.id)).map(x => ( {id: x.id, label: x.name} ))}
                     sx={{ width: 200 }}
                     renderInput={(params) => <TextField {...params} label="Vegetable" />}
                     onChange={selectProduct}
                     value = {product ? ( {id: product.id, label: product.name}) : null}
                 />
                 <OutlinedInput
+                    className="w-1/4"
                     id="outlined-adornment-weight"
-                    endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+                    placeholder='Qty'
+                    endAdornment={<InputAdornment position="end">{product?.measurement}</InputAdornment>}
                     aria-describedby="outlined-weight-helper-text"
                     inputProps={{
                     'aria-label': 'weight',
                     }}
                     value = {quantity}
                     onChange={x => setQuantity(x.target.value)}
+                    type="number"
+                    inputRef={qtyRef}
           />
 
                 <Button variant="contained" onClick={addProductToOrder} disabled = {!product || !quantity}>ADD</Button>
-            </div>
+                </div>
+            </section>
+
+            {/* Error Message */}
             <div className="mt-4">
                 {errorMsg}
             </div>
+
+            {/* Order Table */}
             <div className="mt-8 w-full flex flex-col items-center justify-center">
                 <TableContainer component={Paper} sx={{  maxWidth: 600 }}>
                     <Table  aria-label="simple table">
                         <TableHead>
                         <TableRow>
-                            <TableCell>Name</TableCell>
+                            <TableCell>Vegetable</TableCell>
                             <TableCell >Quantity</TableCell>
                             <TableCell >Action</TableCell>
                         </TableRow>
@@ -135,8 +173,8 @@ export default function Products() {
                             
                                 <TableCell >
                                     <div className="flex flex-row gap-4">
-                                        <Button variant="contained" onClick={x => editProduct(row)}>Edit</Button>
-                                        <Button variant="contained" onClick={x => removeProduct(row.productId)}>Remove</Button>
+                                        {/* <Button variant="contained" onClick={() => editProduct(row)}>Edit</Button> */}
+                                        <Button variant="text" onClick={() => openConfirmDialog(row)}><CancelIcon  color={"error"}/></Button>
                                     </div>
                                 </TableCell>
                             </TableRow>
@@ -145,14 +183,40 @@ export default function Products() {
                     </Table>
                 </TableContainer>
             </div>
+            
             <div className="flex flex-row gap-6 mt-8">
-                <Button variant="outlined" disableElevation>
+                {/* <Button variant="outlined" disableElevation>
                     Clear
-                </Button>
-                <Button variant="contained" disableElevation>
+                </Button> */}
+                <Button variant="contained" disableElevation disabled={orderDetails?.length === 0}>
                     Place Order
-                </Button>
+                </Button> 
             </div>
+
+           
+            {/* Remove Confiramation Dialog */}
+
+            <Dialog
+                open={openDialog}
+                onClose={() => settoRemoveProduct(undefined)}
+                PaperProps={{
+                component: 'form'
+                
+                }}
+            >
+            <DialogTitle>Remove Product</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    You are removing the Product <b>{toremoveProduct?.productName}</b> from your Order.
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+            <Button variant="outlined" onClick={() => { settoRemoveProduct(undefined); setOpenDialog(false) }}>Cancel</Button>
+            <Button variant="contained" onClick={() => removeProduct()}>Remove</Button>
+            </DialogActions>
+      </Dialog>
+           
+            
         </div>
       );
 }
